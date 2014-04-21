e120b5acb31a15bdf01a720b8f8ac5e853656e6b
samuel077 <s750716@gmail.com>
2014-04-22 02:19:07 +0800

    modify news.php controller

--- application/controllers/news.php
+++ application/controllers/news.php
@@ -7,11 +7,6 @@ class News extends CI_Controller {
 		$this->load->model('news_model');
 	}
 
-	public function index()
-	{
-		$data['news'] = $this->news_model->get_news();
-	}
-
 	public function view($slug)
 	{
 		$data['news'] = $this->news_model->get_news($slug);

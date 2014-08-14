3e42c9443a4432c96c1013310eafc718e5592787
samuel077 <s750716@gmail.com>
2014-08-14 17:53:00 +0800

    modify jquery error in header page.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,7 +3,8 @@
 	<meta charset="utf-8">
 	<title><?php echo $title ?> - CodeIgniter 2 Tutorial</title>
 	<link rel="stylesheet" href="<?=base_url("/public/css/bootstrap.min.css")?>">
-	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<!--<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>-->
+	<script src="<?=base_url("/public/js/jquery-1.11.1.min.js")?>"></script>
 	<script src="<?=base_url("/public/js/bootstrap.js")?>"></script>
 <!--	<link rel="stylesheet" href="<?=base_url("/public/css/bootstrap-responsive.min.css")?>"> -->
 </head>
